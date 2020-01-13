e45ecda6e50785a6643fccead4f25a3ffb20d3ec
Adrijana <adrijana.jovicic@gmail.com>
2020-01-13 23:37:13 +0100

    dodala sam novo polje

--- src/probniTestEeee/DistribucijaEps.java
+++ src/probniTestEeee/DistribucijaEps.java
@@ -14,6 +14,7 @@ public class DistribucijaEps {
 	private String naziv;
 	private String adresa;
 	private String telefon;
+	private String nesto;
 	private ArrayList<RacunEps> lista;
 
 	public DistribucijaEps() {
